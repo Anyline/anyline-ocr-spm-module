b23bc3640915e4d8cdd18a29b26d547eddcac9a8
Aldrich Co <aldrich@anyline.com>
2022-06-22 11:44:04 +0200

    Update the Anyline SPM binary to 40.0.0

--- Package.swift
+++ Package.swift
@@ -1,8 +1,6 @@
 // swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
-
 let package = Package(
     name: "AnylinePackage",
     platforms: [.iOS("11")],
@@ -10,6 +8,6 @@ let package = Package(
         .library(name: "AnylinePackage", targets: ["Anyline"]),
     ],
     targets: [
-        .binaryTarget(name: "Anyline", url: "https://anylinesdk.blob.core.windows.net/downloads/Anyline.xcframework-spm-50.0.0.zip", checksum: "16ad96909aa3da0f7e20db1674fd0d2e52452d2945d5c7bcfb9a1dc95e7f6c47")
+        .binaryTarget(name: "Anyline", url: "https://anylinesdk.blob.core.windows.net/downloads/Anyline.xcframework-spm-40.0.0.zip", checksum: "b89400f6ea8b21c8785ea85170165dcfaddc196f3e9197aea5124194b1c4e1e0")
     ]
 )
