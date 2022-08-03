ad2e31c10c8666f747d65e844d87c1f34ce0193c
Aldrich Co <aldrich@anyline.com>
2022-08-03 12:02:37 +0200

    Update the SPM binary to 41.0.0

--- Package.swift
+++ Package.swift
@@ -8,6 +8,6 @@ let package = Package(
         .library(name: "AnylinePackage", targets: ["Anyline"]),
     ],
     targets: [
-        .binaryTarget(name: "Anyline", url: "https://anylinesdk.blob.core.windows.net/downloads/Anyline.xcframework-spm-41.0.0-preview.1.zip", checksum: "8f905604f4eff792dd1ac4660e09bbf5347aaa3080fe382e43f5088e2507c374")
+        .binaryTarget(name: "Anyline", url: "https://anylinesdk.blob.core.windows.net/downloads/Anyline.xcframework-spm-41.0.0.zip", checksum: "4db13e8f8d36138bfc8a836e474ca5fea30a2e332933a7ea78dd941fde99991f")
     ]
 )
