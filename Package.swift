2e70bcae92d3480ba3a0215992aa3b530bc2aa4d
Aldrich Co <aldrich@anyline.com>
2023-06-22 12:50:26 +0200

    Update to 48.0.0

--- Package.swift
+++ Package.swift
@@ -8,6 +8,6 @@ let package = Package(
         .library(name: "AnylinePackage", targets: ["Anyline"]),
     ],
     targets: [
-        .binaryTarget(name: "Anyline", url: "https://anylinesdk.blob.core.windows.net/downloads/Anyline.xcframework-spm-47.0.1.zip", checksum: "cbba5aa30cbd1627603bf35a3cf9ac29ebdcfd792948fec6d537d63dd7b1f756")
+        .binaryTarget(name: "Anyline", url: "https://anylinesdk.blob.core.windows.net/downloads/Anyline.xcframework-spm-48.0.0.zip", checksum: "07fa2ec73d82489c2a10018ed020a9ca40db3a9e2a9d19f2f60548915550790e")
     ]
 )
