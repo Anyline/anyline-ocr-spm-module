68fcc9f41859fb4a28b0cafdfee0a9a3c9d4a2dc
Aldrich Co <aldrich@anyline.com>
2024-03-18 13:41:44 +0100

    39.0.1

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+let package = Package(
+    name: "AnylinePackage",
+    platforms: [.iOS("11")],
+    products: [
+        .library(name: "AnylinePackage", targets: ["Anyline"]),
+    ],
+    targets: [
+        .binaryTarget(name: "Anyline", url: "https://mobile-sdk-ios.anyline.io/spm/Anyline.xcframework-spm-39.0.1.zip", checksum: "4edf31fab0b3a70535e8ea43f8dcdb18d463293c9283d9667211ead65ead83ac")
+    ]
+)
