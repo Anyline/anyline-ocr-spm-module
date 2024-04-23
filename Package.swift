fd0adb6425f87d761f248ba4dd2a266e7f04a640
Aldrich Co <aldrich@anyline.com>
2024-04-23 11:54:19 +0200

    51.5.0

--- Package.swift
+++ Package.swift
@@ -8,6 +8,6 @@ let package = Package(
         .library(name: "AnylinePackage", targets: ["Anyline"]),
     ],
     targets: [
-        .binaryTarget(name: "Anyline", url: "https://mobile-sdk-ios.anyline.io/spm/Anyline.xcframework-spm-51.4.0.zip", checksum: "45df6327a059fc5a2c8e6a0b421f402776ae798753d76399073d9ccc1dcaf1b1")
+        .binaryTarget(name: "Anyline", url: "https://mobile-sdk-ios.anyline.io/spm/Anyline.xcframework-spm-51.5.0.zip", checksum: "cf27e06f5690ad1cbc59625e59bbb2ff761d9fe9ecb89a3a0326b57573aa2b12")
     ]
 )
