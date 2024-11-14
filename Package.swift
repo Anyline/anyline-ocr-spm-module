c58855ce7a39eb2f8d7baeb4fc9f800e087a873f
vimal.venugopalan@anyline.com <>
2024-11-14 21:37:16 +0530

    release/54.4.0

--- Package.swift
+++ Package.swift
@@ -8,6 +8,6 @@ let package = Package(
         .library(name: "AnylinePackage", targets: ["Anyline"]),
     ],
     targets: [
-        .binaryTarget(name: "Anyline", url: "https://mobile-sdk-ios.anyline.io/spm/Anyline.xcframework-spm-54.2.1.zip", checksum: "693c330df3338c14c2cea16b0f0d82dfe440a6208a6285409888cc926f8bfa6a")
+        .binaryTarget(name: "Anyline", url: "https://mobile-sdk-ios.anyline.io/spm/Anyline.xcframework-spm-54.4.0.zip", checksum: "1a43e00b966949c6008e3a893e3d61b3326a62d87a749ecdcbf47ea001bb7fbb")
     ]
 )
