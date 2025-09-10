de445eff4b1c5882a27d06f0560d0b4fa1b94960
Bernd Kampl <bernd.kampl@anyline.com>
2025-09-10 12:07:18 +0200

    fix: fix SPM package checksum

--- Package.swift
+++ Package.swift
@@ -8,6 +8,6 @@ let package = Package(
         .library(name: "AnylinePackage", targets: ["Anyline"]),
     ],
     targets: [
-        .binaryTarget(name: "Anyline", url: "https://mobile-sdk-ios.anyline.io/spm/Anyline.xcframework-spm-55.5.0.zip", checksum: "34c15d6c00911b1236a830db0cc1a8de613006bc63320a4f5a4d9416c2b7bcdf")
+        .binaryTarget(name: "Anyline", url: "https://mobile-sdk-ios.anyline.io/spm/Anyline.xcframework-spm-55.5.0.zip", checksum: "4516499cd21b5e53efa4528c07ce24a5a4122e9761690a6c86a69db52f522728")
     ]
 )
