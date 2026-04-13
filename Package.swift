90ccffaba58abb080af4d8b004d888f09d6f5002
Bernd Kampl <bernd.kampl@anyline.com>
2026-04-13 12:11:01 +0200

    Release 56.0.0 - update checksum

--- Package.swift
+++ Package.swift
@@ -8,6 +8,6 @@ let package = Package(
         .library(name: "AnylinePackage", targets: ["Anyline"]),
     ],
     targets: [
-        .binaryTarget(name: "Anyline", url: "https://mobile-sdk-ios.anyline.io/spm/Anyline.xcframework-spm-56.0.0.zip", checksum: "15e1f2f380c0376db80b4d7e2fc2ead9f39ceaa29aac93e2e41074b0680a7b3d")
+        .binaryTarget(name: "Anyline", url: "https://mobile-sdk-ios.anyline.io/spm/Anyline.xcframework-spm-56.0.0.zip", checksum: "53b78472d847db062442303130d5c6f12d1c522ef6481ce88c3aa34b36567f49")
     ]
 )
